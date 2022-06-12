6736f4cbb20c2517ba34416e14e1ff015c3494e5
imdimasan <info@starlink.by>
2022-06-12 23:00:54 +0300

    INV-05 Fixing cookie setting bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,7 +22,7 @@ class UserController {
             const {email, password} = req.body
             const userData = await userService.login(email, password)
             console.log("COOCKIE", res)
-            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, secure: true, domain: '.invoice.by'})
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000})
             return res.json(userData)
             
         } catch (error) {
