6392c0a89a43fe0d9685f3fb9fde463307b94db4
imdimasan <info@starlink.by>
2022-06-12 23:22:40 +0300

    INV-05 Fixing cookie setting bug

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: 'invoice.by',
+    origin: process.env.CLIENT_URL,
     credentials: true,
   }));
 app.use('/api', routes)
