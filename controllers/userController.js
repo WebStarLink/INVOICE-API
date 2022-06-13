c5aea2a01775b80ae04874011d73deee18b2e06b
imdimasan <info@starlink.by>
2022-06-13 21:18:40 +0300

    INV-05 Fixing cookie setting bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,7 +11,7 @@ class UserController {
             }
             const {email, password} = req.body
             const userData = await userService.registration(email, password)
-            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'none', secure: true})
             return res.json(userData)
         } catch (error) {
             next(error);
@@ -21,7 +21,7 @@ class UserController {
         try {
             const {email, password} = req.body
             const userData = await userService.login(email, password)
-            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: false})
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'none', secure: true})
             console.log("REQ COOKIE", req.cookie);
             console.log("DOMAIN", process.env.CLIENT_URL)
             console.log("RES COOKIE", req.cookie);
@@ -54,7 +54,7 @@ class UserController {
         try {
             const {refreshToken} = req.cookies
             const userData = await userService.refresh(refreshToken)
-            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'none', secure: true})
             return res.json(userData)
         } catch (error) {
             next(error)
